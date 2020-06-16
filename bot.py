9f8212172dbb262b463cf2b632fb789688e223d5
Silvano H <40663430+silvanohirtie@users.noreply.github.com>
2020-06-16 12:02:32 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,8 +11,9 @@ import time
 botCount = 0
 counter = 0
 chooseAgent = ""
+current_path = current_path
 
-fireUrl = input("URL To fire: ")
+url = input("URL to visit: ")
 agents = ["user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
           "user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36",
           "user-agent=Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_6; en-en) AppleWebKit/533.19.4 (KHTML, like Gecko) Version/5.0.3 Safari/533.19.4",
@@ -34,10 +35,10 @@ while True: #Loop
     if(agentPrint == "Random"):
         chooseAgent = random.choice(agents) #Take random choice from the agents array
     print(chooseAgent)
-    print("Url: ",fireUrl)
+    print("Url: ",url)
     botCount +=1
     print("Bot N.",botCount)
-    lines = open(os.path.dirname(os.path.realpath(__file__))+'/proxies.txt').read().splitlines() #Open the proxies.txt file and read all the lines
+    lines = open(current_path+'/proxies.txt').read().splitlines() #Open the proxies.txt file and read all the lines
     chooseProxy = random.choice(lines) #Choose a random line
     print("Proxy: ",chooseProxy)
 
@@ -47,10 +48,10 @@ while True: #Loop
     opts.add_experimental_option('excludeSwitches', ['enable-logging']) #Disable useless logs in the console
     opts.add_argument('--proxy-server=%s' % chooseProxy) #Set proxy
     opts.add_argument('headless') #Make it run in background
-    driver = webdriver.Chrome(os.path.dirname(os.path.realpath(__file__))+"/chromedriver.exe",options=opts) #Select WebDriver
+    driver = webdriver.Chrome(current_path+"/chromedriver.exe",options=opts) #Select WebDriver
     driver.set_page_load_timeout(15) #Setting a 15 seconds timeout
     try:
-        driver.get(fireUrl) #Fire the url
+        driver.get(url) #Fire the url
         time.sleep(2) #Wait 2 seconds
     except TimeoutException as ex:
         print("Proxy is taking too much time...")
@@ -59,5 +60,5 @@ while True: #Loop
     driver.delete_all_cookies() #Clean the cookies
     print("Cookie cleaned, exiting..\n")
     driver.quit() #exit
-    if counter > 100:
+    if counter > maxloop:
         break #After 100 attemps, stop the code
